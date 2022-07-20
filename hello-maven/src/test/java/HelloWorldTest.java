ab5f0db8a58be4ad1649183541d4723807be74a2
Thế <theptit01@gmail.com>
2022-07-20 15:43:29 +0700

    up code

---
+++ hello-maven/src/test/java/HelloWorldTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+    @Test
+    public void testHello() {
+        System.out.println("TEST - Hello...");
+    }
+
+    @Test
+    public void testWorld() {
+        System.out.println("TEST - World!!!");
+    }
+}
